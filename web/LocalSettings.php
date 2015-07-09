3ba4ed16c336ef9336f8b610e0b8e8ffd1407468
Chris Smith <chris@cs278.org>
2015-07-09 20:23:21 +0100

    Enable URL rewriting

--- web/LocalSettings.php
+++ web/LocalSettings.php
@@ -37,10 +37,12 @@ $wgSitename         = "WikiLEC";
 
 ## The URL base path to the directory containing the wiki;
 ## defaults for all runtime URL paths are based off of this.
-$wgScriptPath       = "";
-
 ## For more information on customizing the URLs please see:
 ## http://www.mediawiki.org/wiki/Manual:Short_URL
+$wgScriptPath = "";
+$wgScriptExtension = ".php";
+$wgArticlePath = "/view/$1";
+$wgUsePathInfo = true;
 
 $wgEnableEmail      = true;
 $wgEnableUserEmail  = true;
